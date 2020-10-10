d7fd7639ebc79cb3a3c031994fae99a7b76d3781
alibaba0507 <fx2go4u@gmail.com>
2020-10-11 00:07:55 +0200

    Signed-off-by: alibaba0507 <fx2go4u@gmail.com>

--- bot-detect.php
+++ bot-detect.php
@@ -4,11 +4,22 @@
 	require_once 'adm/vendor/autoload.php';
 	use hisorange\BrowserDetect\Parser as Browser;
 	$browser = new Browser;
+	/*
+	This to be used be JSONP where is a cross domain js script
+	 to execute this on the client site add this code
+	 <script>
+		function isBot(data){ 
+		    // do someting with data
+			//document.getElementById("info").textContent= data.name;
+		};
+	</script>
+	<script src="https://bot-detect-1.herokuapp.com/bot-detect.php"></script>
+	*/
 	if ($browser->isBot()){
-	  echo "isBot({name:1});";
+	  echo "isBot({1});";
 	} else
 	{
-		echo "isBot({name:0});";
+		echo "isBot({0});";
 	}
 	die();
 ?>   
